af0f19731e000a19ffb9b3628104858f8bce84f1
Bulat <rbulatych@gmail.com>
2024-10-04 23:13:07 +0300

    Add two additional modules

---
+++ additional_modules/dna_rna_tools.py
@@ -0,0 +1,44 @@
+
+def valid_sequence(seqs):
+    for nucl_acid in seqs:
+        t_in_nucl_acid = "T" in nucl_acid or "t" in nucl_acid
+        u_in_nucl_acid = "U" in nucl_acid or "u" in nucl_acid
+        if t_in_nucl_acid and u_in_nucl_acid:
+            return False
+    return True
+
+
+def seq_transcr(seq):
+    return  seq.replace('T', 'U').replace('t', 'u')
+
+
+def seq_reverse(seq):
+    return seq[::-1]
+
+def seq_compl(seq):
+    dna_complement_dict = {
+        'A' : 'T',
+        'T' : 'A',
+        'G' : 'C',
+        'C' : 'G',
+        'a' : 't',
+        't' : 'a',
+        'c' : 'g',
+        'g' : 'c',
+        'U' : 'A',
+        'u' : 'a'
+        
+    }
+    
+    return ''.join(dna_complement_dict[base] if base in dna_complement_dict else base for base in seq)
+
+
+
+def seq_rev_compl(seq):
+    return seq_reverse(seq_compl(seq))
+
+if __name__ == "__main__":
+    seqs = input('Введите последовательность нуклеиновой кислоты: ').split()
+    procedure = input('Введите тип операции: ')
+    result = run_dna_rna_tools(*seqs, procedure)
+    print(result)
---
+++ additional_modules/filter_fastq.py
@@ -0,0 +1,32 @@
+from typing import  Tuple, Union, Dict
+def gc_content_calculator(sequence):
+    length_sequence = len(sequence)
+    if length_sequence == 0:
+        return 0.0
+    gc_count = sequence.count('G') + sequence.count('C') + sequence.count('g') + sequence.count('c')
+    if sequence:
+        return gc_count/length_sequence * 100
+    else:
+        return 0
+
+
+def check_length_bounds(length: int, length_bounds: Tuple[int, int]):
+    return length_bounds[0] <= length <= length_bounds[1]
+
+
+def quality_check(quality):
+    if len(quality) == 0:
+        return 0.0
+    total_quality = 0
+    for char in quality:
+        total_quality += ord(char) - 33
+    return total_quality/len(quality)
+
+
+
+
+
+
+
+
+
