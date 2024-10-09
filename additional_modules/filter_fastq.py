3666842c38ee8c16fd261b946851d26b97b08172
Bulat <rbulatych@gmail.com>
2024-10-09 15:53:56 +0300

    Add new additional functions

--- additional_modules/filter_fastq.py
+++ additional_modules/filter_fastq.py
@@ -1,3 +1,4 @@
+from tabnanny import check
 from typing import Tuple
 
 
@@ -21,10 +22,22 @@ def is_length_bounds(length: int, length_bounds: Tuple[int, int]) -> bool:
     return length_bounds[0] <= length <= length_bounds[1]
 
 
-def quality_check(quality: str) -> float:
+def calc_quality(quality: str) -> float:
     if len(quality) == 0:
         return 0.0
     total_quality = 0
     for char in quality:
         total_quality += ord(char) - 33
     return total_quality / len(quality)
+
+def quality_check(quality: str, quality_threshold: int) -> bool:
+    avg_quality = calc_quality(quality)
+    return avg_quality >= quality_threshold
+
+def check_length(length_bounds: Tuple[int, int], sequence: str) -> bool:
+    return is_length_bounds(len(sequence), length_bounds)
+
+
+def check_gc_content(gc_bounds: Tuple[float, float], sequence: str) -> bool:
+    gc_content = gc_content_calculator(sequence)
+    return gc_bounds[0] <= gc_content <= gc_bounds[1]
