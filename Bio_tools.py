a31e133a873604017d7c7902fe1bee213d15e2ac
Bulat <rbulatych@gmail.com>
2025-02-25 02:18:24 +0300

    Make task2

--- Bio_tools.py
+++ Bio_tools.py
@@ -1,4 +1,12 @@
 from abc import ABC, abstractmethod
+import os
+from os import write
+from Bio import SeqIO
+from Bio.Seq import Seq
+from Bio.SeqRecord import SeqRecord
+from Bio.SeqUtils import gc_fraction
+from typing import Dict, Union, Tuple
+
 
 
 class BiologicalSequence(ABC):
@@ -23,9 +31,9 @@ class BiologicalSequence(ABC):
     def _check_alphabet(self) -> bool:
         return all(base in self.alphabet for base in self.sequence)
 
-    @abstractmethod
-    def complement(self):
-        pass
+    # @abstractmethod
+    # def complement(self):
+    #     pass
     @abstractmethod
     def reverse(self):
         pass
@@ -71,10 +79,10 @@ class RNASequence(NucleicAcidSequence):
 
 
 class AminoAcidSequence(BiologicalSequence):
-    def _check_alphabet(self) -> bool:
-        amino_acids = set("ACDEFGHIKLMNPQRSTVWY")
-        return all(residue in amino_acids for residue in self.sequence)
-
+    def __init__(self, sequence: str):
+        super().__init__(sequence, { 'A', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'K', 'L', 'M', 'N',
+                                   'P', 'Q', 'R', 'S', 'T', 'V', 'W', 'Y'
+})
     def reverse(self):
         return self.sequence[::-1]
 
@@ -89,3 +97,46 @@ class AminoAcidSequence(BiologicalSequence):
             else:
                 amino_count[amino] = 1
         return amino_count
+
+    def _check_alphabet(self) -> bool:
+        amino_acids = set("ACDEFGHIKLMNPQRSTVWY")
+        return all(residue in amino_acids for residue in self.sequence)
+
+
+
+def filter_fastq(input_fastq: str,
+    output_fastq: str,
+    gc_bounds: Union[Tuple[float, float], float] = (0, 100),
+    length_bounds: Union[Tuple[int, int], int] = (0, 2**32),
+    quality_threshold: int = 0,
+) -> Dict[str, Tuple[str, str]]:
+
+    if isinstance(gc_bounds, (int, float)):
+        gc_bounds = (0, gc_bounds)
+    if isinstance(length_bounds, (int)):
+        length_bounds = (0, length_bounds)
+
+    output_dir = "filtered"
+    if not os.path.exists(output_dir):
+        os.makedirs(output_dir)
+    output_path = os.path.join(output_dir, output_fastq)
+
+    with open(input_fastq, 'r') as file_in, open(output_path, 'w') as file_out:
+        for record in SeqIO.parse(file_in, 'fastq'):
+            sequence = record.seq
+            quality = record.letter_annotations.get('phred_quality', [])
+
+            if not (length_bounds[0] <= len(sequence) <= length_bounds[1]):
+                continue
+
+            avg_quality = sum(quality) / len(quality) if quality else 0
+            if avg_quality < quality_threshold:
+                continue
+            gc_content = gc_fraction(sequence) * 100
+            if not (gc_bounds[0] <= gc_content <= gc_bounds[1]):
+                continue
+
+            SeqIO.write(record, file_out, 'fastq')
+
+    print(f"All sequences were filtered and saved in: '{output_path}'.")
+
