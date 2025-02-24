cd645fc6fdb7018fc1e30db24768e23d13d3c05b
Bulat <rbulatych@gmail.com>
2025-02-24 23:27:55 +0300

    Update task1

--- Bio_tools.py
+++ Bio_tools.py
@@ -21,16 +21,17 @@ class BiologicalSequence(ABC):
         return f"{self.__class__.__name__}('{self.sequence}')"
 
     def _check_alphabet(self) -> bool:
-        """Проверка, что все символы последовательности присутствуют в алфавите."""
         return all(base in self.alphabet for base in self.sequence)
 
     @abstractmethod
     def complement(self):
         pass
-
+    @abstractmethod
+    def reverse(self):
+        pass
 
 class NucleicAcidSequence(BiologicalSequence):
-    complement_map = {}
+    complement_map = {} # for polymorphism
 
     def complement(self):
         if not self.complement_map:
@@ -45,7 +46,7 @@ class NucleicAcidSequence(BiologicalSequence):
 
 
 class DNASequence(NucleicAcidSequence):
-    complement_map = {"A": "T", "T": "A", "C": "G", "G": "C"}
+    complement_map = {"A": "T", "T": "A", "C": "G", "G": "C"} # for polymorphism
 
     def __init__(self, sequence: str):
         super().__init__(sequence, {"A", "T", "C", "G"})
@@ -55,25 +56,36 @@ class DNASequence(NucleicAcidSequence):
 
 
 class RNASequence(NucleicAcidSequence):
-    complement_map = {"A": "U", "U": "A", "C": "G", "G": "C"}
-
+    complement_map = {"A": "U", "U": "A", "C": "G", "G": "C"} # for polymorphism
+    stop_codons = {"UAA", "UAG", "UGA"}
     def __init__(self, sequence: str):
         super().__init__(sequence, {"A", "U", "C", "G"})
 
+    def find_stop_codons(self, rna_sequence: str):
+        stop_positions = []
+        for i in range(0, len(rna_sequence) - 2, 3):
+            codon = rna_sequence[i:i + 3]
+            if codon in self.stop_codons:
+                stop_positions.append(i)
+        return stop_positions
+
 
 class AminoAcidSequence(BiologicalSequence):
-    def __init__(self, sequence: str):
-        # Предположим, что алфавит аминокислот - это стандартный набор 20 аминокислот
-        amino_acid_alphabet = set("ACDEFGHIKLMNPQRSTVWY")
-        super().__init__(sequence, amino_acid_alphabet)
+    def _check_alphabet(self) -> bool:
+        amino_acids = set("ACDEFGHIKLMNPQRSTVWY")
+        return all(residue in amino_acids for residue in self.sequence)
 
+    def reverse(self):
+        return self.sequence[::-1]
 
-# class AminoAcidSequence(BiologicalSequence):
-#     def __init__(self,sequence):
-#         super().__init__(sequence)
+    def find_aromatic_acid(self, amino):
+        return amino in {'F', 'W', 'Y'}
 
-rna = RNASequence('AGCA')
-rna.reverse_complement()
-print(rna.reverse_complement())
-dna = DNASequence('AT')
-print(dna.reverse())
+    def count_repeating_amino(self, amino_sequence):
+        amino_count = {}
+        for amino in amino_sequence:
+            if amino in amino_count:
+                amino_count[amino] += 1
+            else:
+                amino_count[amino] = 1
+        return amino_count
